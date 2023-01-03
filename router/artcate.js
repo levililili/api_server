bda9db5edea0251ec6ab6063349699120ecb3f8b
levililili <593494294@qq.com>
2023-01-04 01:40:58 +0800

    完成--新增文章分类

--- router/artcate.js
+++ router/artcate.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const router = express.Router()
-
 // 导入文章分类的路由处理函数模块
 const artcate_handler = require('../router_handler/artcate')
+// 导入验证规则的中间件
+const expressJoi = require('@escook/express-joi')
+// 导入验证规则模块
+const { add_cate_schema } = require('../schema/article')
 
 // 挂载路由
 // 获取文章分类信息
 router.get('/cates', artcate_handler.getArticleCates)
+// 新增文章分类
+router.post('/addcates', expressJoi(add_cate_schema), artcate_handler.updateArticleCates)
 
 module.exports = router
--- router_handler/artcate.js
+++ router_handler/artcate.js
@@ -17,3 +17,32 @@ exports.getArticleCates = (req, res) => {
 
 }
 
+// 新增文章分类
+exports.updateArticleCates = (req, res) => {
+    // 查询分类名称和别名是否被占用
+    const sql = 'select * from ev_article_cate where name=? or alias=?'
+    db.query(sql, [req.body.name, req.body.alias], (err, results) => {
+        if (err)
+            return res.cc(err)
+        // 分类名称 和 分类别名 都被占用
+        // --分类名称 和 分类别名 分别被两行占用
+        if (results.length === 2) return res.cc('分类名称与别名被占用，请更换后重试！')
+        // --分类名称 和 分类别名 同时被一行占用
+        if (results.length === 1 && results[0].name === req.body.name && results[0].alias === req.body.alias) return res.cc('分类名称与别名被占用，请更换后重试！')
+        // 分类名称 或 分类别名 被占用
+        if (results.length === 1 && results[0].name === req.body.name) return res.cc('分类名称被占用，请更换后重试！')
+        if (results.length === 1 && results[0].alias === req.body.alias) return res.cc('分类别名被占用，请更换后重试！')
+
+        // 新增文章分类
+        const sql = 'insert into ev_article_cate set ?'
+        db.query(sql, req.body, (err, results) => {
+            if (err)
+                return res.cc(err)
+            if (results.affectedRows !== 1)
+                return res.cc('新增文章分类失败')
+            res.cc('新建文章分类成功', 0)
+        })
+
+    })
+}
+
